5ee988c7ec622e905a14753cd4851f9008017dd5
onizukaTP <tharunprabu2418@gmail.com>
2026-01-11 19:34:57 +0530

    Add solution for Leetcode 283: Move Zeroes

---
+++ dsa/leetcode/array/MoveZeroes.java
@@ -0,0 +1,27 @@
+/**
+ * #283 Leetcode
+ * 
+ * Approach 
+ * - Two pointers with conditional advance and swapping
+ * 
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+*/
+
+package dsa.leetcode.array;
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i] == 0 && nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            } else if (nums[i] != 0 && nums[j] == 0) i++;
+            else j++;
+        }
+    }
+}

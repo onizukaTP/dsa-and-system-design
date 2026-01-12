4b6ea4e2b7eabcfcb5ed77f7481887441e283df7
onizukaTP <tharunprabu2418@gmail.com>
2026-01-12 17:41:28 +0530

    Add solution for Leetcode 344: Reverse String

---
+++ dsa/leetcode/array/ReverseString.java
@@ -0,0 +1,27 @@
+/**
+ * #344 Leetcode: Reverse string
+ * 
+ * Approach 
+ * - Use two pointers: one at the beginning and one at the end.
+ * - Swap both and increment left pointer and decrement right pointer.
+ * - Do this till the left pointer is less than the right pointer.
+ * 
+ * Time complexity: O(n)
+ * Space complexity: O(1)
+*/
+
+package dsa.leetcode.array;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}

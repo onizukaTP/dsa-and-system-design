b2a8497627255b4f1ad3a84641c01c50e943e15d
onizukaTP <tharunprabu2418@gmail.com>
2026-01-12 17:32:59 +0530

    Add solution for Leetcode 66: Plus One

---
+++ dsa/leetcode/array/PlusOne.java
@@ -0,0 +1,35 @@
+/**
+ * #66 Leetcode: Plus One
+ * 
+ * Approach 
+ * - Traverse the array from right to left
+ * - If the digit is less than 9, increment it and return
+ * - If the digit is 9, set it to 0 and continue carrying
+ * - If all digits are 9, create a new array with a leading 1
+ * 
+ * Time complexity: O(n)
+ * Space complexity: O(n) in the worst case, O(1) otherwise
+*/
+
+package dsa.leetcode.array;
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length - 1;
+        for (int j = n; j >= 0; j--) {
+            if (digits[j] == 9) {
+                digits[j] = 0;
+                if (j == 0) {
+                    int[] res = new int[digits.length + 1];
+                    res[0] = 1;
+                    return res;
+                }
+            }
+            else {
+                digits[j] += 1;
+                break;
+            }
+        }
+        return digits;
+    }
+}

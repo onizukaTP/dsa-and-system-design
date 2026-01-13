f52728a4dc763611fa81a380f14b50566fceb8b8
onizukaTP <tharunprabu2418@gmail.com>
2026-01-13 18:34:08 +0530

    Add solution for Leetcode 1: Two sum - Brute force

---
+++ dsa/leetcode/array/TwoSum.java
@@ -0,0 +1,22 @@
+/**
+ * #1 Leetcode: Two Sum
+ * 
+ * 1. Approach - Brute Force
+ * 
+ * Time complexity: O(n^2)
+ * Space complexity: O(1)
+*/
+
+package dsa.leetcode.array;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] {i, j};
+            }
+        }
+        return new int[2];
+    }
+}

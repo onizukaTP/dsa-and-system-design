c07a6c598932124b9cae3da090a3a4cdbf538199
onizukaTP <tharunprabu2418@gmail.com>
2026-01-13 18:43:37 +0530

    Refactor solution for Leetcode 1: Two Sum - Optimal

--- dsa/leetcode/array/TwoSum.java
+++ dsa/leetcode/array/TwoSum.java
@@ -1,22 +1,30 @@
 /**
  * #1 Leetcode: Two Sum
  * 
- * 1. Approach - Brute Force
+ * Approach (Optimal):
+ * - Use a HashMap to store each number as the key and its index as the value
+ * - For each element, check if its complement (target - current number) exists in the map
+ * - If found, return the indices of the complement and the current element
+ * - Otherwise, store the current number and its index in the map
  * 
- * Time complexity: O(n^2)
- * Space complexity: O(1)
+ * Time complexity: O(n)
+ * Space complexity: O(n)
 */
 
 package dsa.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target)
-                    return new int[] {i, j};
-            }
+            int complement = target - nums[i];
+            if (map.containsKey(complement))
+                return new int[] {map.get(complement), i};
+            map.put(nums[i], i);
         }
-        return new int[2];
+        throw new IllegalArgumentException("No solution");
     }
 }

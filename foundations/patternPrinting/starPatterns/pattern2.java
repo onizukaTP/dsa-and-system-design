0d598aa2a2de1233bedbaa6e2bdd7e78beea34fb
onizukaTP <tharunprabu2418@gmail.com>
2026-01-17 13:05:11 +0530

    Add Pattern 2: inverted right-angled start triangle

---
+++ foundations/patternPrinting/starPatterns/pattern2.java
@@ -0,0 +1,22 @@
+package foundations.patternPrinting.starPatterns;
+
+// *****
+// ****
+// ***
+// **
+// *
+
+public class pattern2 {
+    public static void main(String[] args) {
+        printPattern2(5);
+    }
+
+    static void printPattern2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
